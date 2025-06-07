22af69ed2f341a3f612a843c8326ab2440cb2b23
egogoboy <117520118+egogoboy@users.noreply.github.com>
2025-06-07 19:10:03 +0300

    Delete build directory

--- build/CMakeFiles/MySFMLProject.dir/src/appWindow.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/MySFMLProject.dir/src/appWindow.cpp.o: \
- /home/egogoboy/Downloads/Telegram\ Desktop/course_work_1st_course/src/appWindow.cpp \
- /usr/include/stdc-predef.h \
- /home/egogoboy/Downloads/Telegram\ Desktop/course_work_1st_course/include/appWindow.h
